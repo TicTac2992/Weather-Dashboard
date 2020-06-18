006f6ac936bc51324ad480e55bfb6d08342d8c46
Jared Varnum <jvarnum3@gmail.com>
2020-06-18 15:20:23 -0500

    adding in links and header/nav bar

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+    $.ajax({
+      url: "https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy",
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
