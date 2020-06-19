c3cb8424e6d4f57e804b3f5f51c1af2aad2aac23
Jared Varnum <jvarnum3@gmail.com>
2020-06-18 22:37:46 -0500

    adding in additional HTML elements, and some variables into .js file

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
+// global variables
+var cityName = '';
+var date = '';
+var humidity = '';
+var icon = ''; 
+var temp = '';
+var uvIndex = '';
+var windSpeed = ''; 
+
+
+var APIKey = "8a1accb502972b20f4e96b06ebea2973";
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Tampa,Florida&appid=" + APIKey;
+
 
 
 
 
     $.ajax({
-      url: "https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy",
+      url: "https://api.openweathermap.org/data/2.5/weather?",
       method: "GET"
     }).then(function(response) {
       console.log(response);
